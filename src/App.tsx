1e0eb5836eb803e55b3f80ad52d376824f0e179b
abdujamil gafarov <gafforovabdujamil@gmail.com>
2021-08-04 11:24:58 +0500

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import Home from './Components/Home';
+import Register from './Components/Register';
 
 function App() {
-  return <div></div>;
+  return (
+    <HashRouter>
+      <div>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/home" component={Home} />
+        </Switch>
+      </div>
+    </HashRouter>
+  );
 }
 
 export default App;
